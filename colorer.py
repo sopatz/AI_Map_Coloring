823f01cd72436303ea33b53d45d9a1b29d05a5ff
sopatz <sopatz04@gmail.com>
2025-11-04 21:00:51 -0500

    county color assignment functionality
    Uses most-restrained variable, then most-restraining variable in case of ties, to assign a color to each county for the selected state

--- colorer.py
+++ colorer.py
@@ -1,5 +1,4 @@
 import csv
-import math
 
 county_borders = []
 states = set()
@@ -94,3 +93,105 @@ ranked_counties = sorted(border_counts.items(), key=lambda x: x[1], reverse=True
 print("\nCounties ranked by number of borders:")
 for county_id, count in ranked_counties:
     print(f"{county_id}: {count} borders")
+
+
+#model county borders as graph-style adjacency list for easier usage
+adj_list = {county: set() for county in counties_in_state}
+
+for row in state_county_borders:
+    county1 = row["County GEOID"]
+    county2 = row["Neighbor GEOID"]
+    adj_list[county1].add(county2)
+    adj_list[county2].add(county1)
+
+print(adj_list)
+
+
+# Keep track of final assigned colors
+final_colors = {}
+
+# Check if assigning a color to a county is valid given the county's neighbors
+def is_valid_color(county, color):
+    # Loop over every neighbor of the input county
+    for neighbor in adj_list[county]:
+        # Check if neighboring county has been assigned the color we are testing
+        if final_colors.get(neighbor) == color:
+            return False
+    return True
+
+# Choose the next county to color using Most Restrained Variable, then Most Restraining Variable
+def select_next_county():
+    # Creates list of tuples containing: county, number of color options, and border count
+    uncolored = [
+        (county, len(county_colors[county]), border_counts[county])
+        for county in counties_in_state if county not in final_colors
+    ]
+    # Signal to recursion that every county is colored (uncolored is empty)
+    if not uncolored:
+        return None  #signal that all counties are colored
+    # Sort by number of options (ascending), then number of borders (descending)
+    uncolored.sort(key=lambda x: (x[1], -x[2], x[0]))
+    # Return first county in sorted uncolored county list, as this is the one we now choose to color
+    return uncolored[0][0]
+
+# Recursive function that performs map coloring with backtracking
+# Returns True if all counties were successfully colored
+# Returns False if current county color assignment doesn't meet constraints --> backtrack to last assignment
+def color_counties():
+    # Base case: all counties have been colored
+    if len(final_colors) == len(counties_in_state):
+        return True
+
+    # Select county to try coloring using the heuristic outlined in "select_next_county()" function
+    county = select_next_county()
+    # Check if county == None
+    if not county:
+        return True  # no uncolored counties left
+
+    # Iterate through available colors for the selected county (ascending)
+    for color in sorted(county_colors[county]):
+        # Confirm color is conflict-free with currently assigned neighbors
+        if is_valid_color(county, color):
+            final_colors[county] = color # assign color to selected county
+
+
+            # Temporarily reduce neighbor color options for pruning:
+
+            # dict to hold the neighbors which have had the chosen color removed, so it can be restored on backtrack
+            removed = {} 
+
+            # Loop through all of the current county's neighbors
+            for neighbor in adj_list[county]:
+                # Check if neighbor is uncolored and the chosen color is in their available set
+                if neighbor not in final_colors and color in county_colors[neighbor]:
+                    # Remove chosen color from neighbor's available set of colors
+                    county_colors[neighbor].remove(color)
+
+                    # Record the removal in "removed" so that it can be restored on backtrack
+                    removed[neighbor] = color
+
+            # Recursive call
+            if color_counties():
+                return True  # stop iteration if full solution was found
+
+            # If recursive call fails:
+
+            # Undo assignment
+            del final_colors[county]
+            # Restore neighbors' color options that got removed
+            for neighbor in removed:
+                county_colors[neighbor].add(color)
+
+    # No color worked --> backtrack to undo last assignment and try alternatives
+    return False
+
+
+print("\nColoring counties...")
+success = color_counties()  # Call backtracking county-coloring function
+
+if success:
+    print("\n Successfully colored all counties!")
+else:
+    print("\n Could not color all counties with the chosen number of colors.")
+
+print(final_colors)
